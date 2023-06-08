79d18ea4a5bc70a6772b205e676e2db55ddf9733
ViniciusLima115 <mankindbr@gmail.com>
2023-06-08 12:20:42 -0700

    Criando FAQ e Footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
   
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener('click', function(botao) { 
@@ -9,10 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
         hideAllTabs();
         tab.classList.add('shows__list--is--active');
         removeActiveButton();
-        event.target.classList.add('shows__tabs__button--is--active');
+        botao.target.classList.add('shows__tabs__button--is--active');
       });
     }
+
+    for(let i = 0; i < questions.length; i++){
+      questions[i].addEventListener('click',openAndClose);
+    }
   });
+
+  function openAndClose (element) {
+    const classe = 'faq__questions__item--is--open';
+    const parentElement = element.target.parentNode;
+
+    parentElement.classList.toggle(classe);
+
+  }
   
   function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');

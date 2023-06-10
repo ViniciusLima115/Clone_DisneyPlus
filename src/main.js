e356ebdf8045c732c9f766893496771368db713c
ViniciusLima115 <mankindbr@gmail.com>
2023-06-09 17:24:24 -0700

    ajuste na responsividade

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll',function(){
+       const posicaoAtual = window.scrollY;
+
+       if(posicaoAtual < alturaHero){
+            hideHeaderElements();
+       }else{
+        showHeaderElements();
+       }
+    })
   
+
+    //SECAO DE ATRACOES, PROGRAMACAO DAS ABAS
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener('click', function(botao) { 
         const targetTab = botao.target.dataset.tabButton; 
@@ -13,11 +28,21 @@ document.addEventListener('DOMContentLoaded', function() {
         botao.target.classList.add('shows__tabs__button--is--active');
       });
     }
-
+    // SECAO FAQ, ACCORDION
     for(let i = 0; i < questions.length; i++){
       questions[i].addEventListener('click',openAndClose);
     }
   });
+
+  function hideHeaderElements(){
+    const header = document.querySelector('header')
+    header.classList.add('header--is--hidden');
+  }
+
+  function showHeaderElements(){
+    const header = document.querySelector('header')
+    header.classList.remove('header--is--hidden');
+  }
 
   function openAndClose (element) {
     const classe = 'faq__questions__item--is--open';
